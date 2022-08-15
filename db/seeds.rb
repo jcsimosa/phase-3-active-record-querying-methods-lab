a02031ae2ae2234d5ce63ba1b1f698810a1a020d
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-08-15 16:35:11 -0500

    commit tree a84fb11d0641ba48a6a9b30834d18948d01e4224

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+puts "Seeding"
+
+Show.create(name: "friends", network: "WB", day: "monday", rating: 10, season: "2")
+Show.create(name: "Batman", network: "DC", day: "sunday", rating: 7, season: "1")
+Show.create(name: "two and half men", network: "WB", day: "thursday", rating: 10, season: "9")
+Show.create(name: "celebrity deathmach", network: "Mtv", day: "saturday", rating: 8, season: "5")
+Show.create(name: "Breaking Bad", network: "AMC", day: "Tueday", rating: 10, season: "6")
+
+puts "Done seeding"
